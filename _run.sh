154805cf1dcd9bce4ebb1b9cde5d6dd431068872
JGI <jgi@eury.jgwill.com>
2025-05-31 14:24:34 -0400

    fix

--- _run.sh
+++ _run.sh
@@ -1,6 +1,7 @@
 FLOWISE_DIR=$HOME/.flowise.3.dev
 MOUNT_PATH=/root/.flowise
 XPORT=3003
+. /opt/binscripts/load.sh
 
-dkrcrm flowise
+dkcrm flowise
 docker run -d --name flowise -v $FLOWISE_DIR:$MOUNT_PATH -p $XPORT:3000 flowise
